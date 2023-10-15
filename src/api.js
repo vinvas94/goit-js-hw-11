d1a48b571a1164eb958481631c6150e64320d447
vinvas94 <vintoniv1111@gmail.com>
2023-10-15 22:49:19 +0200

    todo

---
+++ src/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const API_KEY = '40010712-6d7af93e262d6e116d716f3d5';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    Notiflix.Notify.failure('Something went wrong. Please try again later.');
+    return Promise.reject(error);
+  }
+);
+
+async function getImages(query, page, perPage) {
+  const response = await axios.get(
+    `?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+  );
+  return response.data;
+}
+
+export { getImages };
---
+++ src/index.js
@@ -0,0 +1,142 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { getImages } from './api';
+
+let page = 1;
+let query = null;
+let perPage = 40;
+
+let simpleLightbox;
+let loadedImages = [];
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  buttonLoad: document.querySelector('.load-more'),
+  list: document.querySelector('.gallery'),
+};
+
+function createMarkup(arr) {
+  if (!refs.list) {
+    return;
+  }
+  const markup = arr
+    .map(arr => {
+      const {
+        id,
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = arr;
+
+      return `<div class="photo-card" id="${id}">
+  <img src="${webformatURL}" alt="${tags}" loading="${largeImageURL}" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>: ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>: ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>: ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>: ${downloads}
+    </p>
+  </div>
+</div>`;
+    })
+
+    .join('');
+  refs.list.insertAdjacentHTML('beforeend', markup);
+}
+
+refs.form.addEventListener('submit', onSearch);
+
+function onSearch(evt) {
+  evt.preventDefault();
+
+  page = 1;
+  refs.list.innerHTML = '';
+  query = evt.currentTarget.searchQuery.value.trim();
+
+  if (query === '') {
+    Notiflix.Notify.failure('Please specify your search query');
+    return;
+  }
+
+  getImages(query, page, perPage)
+    .then(data => {
+      if (data.hits.length * page === data.totalHits) {
+        refs.buttonLoad.classList.add('hide');
+      } else {
+        refs.buttonLoad.classList.remove('hide');
+      }
+      if (data.totalHits === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        createMarkup(data.hits);
+        simpleLightboxList();
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => refs.form.reset());
+}
+
+createMarkup(loadedImages);
+function simpleLightboxList() {
+  if (simpleLightbox) {
+    simpleLightbox.refresh();
+  } else {
+    simpleLightbox = new SimpleLightbox('.gallery', {
+      captionsData: 'alt',
+      captionDelay: 250,
+      enableKeyboard: true,
+    });
+  }
+}
+
+// refs.buttonLoad.addEventListener('click', onButtonLoad);
+// let loading = false;
+
+async function onButtonLoad() {
+  // if (loading) {
+  //   return;
+  // }
+  // loading = true;
+  page += 1;
+  const data = await getImages(query, page, perPage);
+
+  if (perPage * page >= data.totalHits) {
+    refs.buttonLoad.classList.add('hide');
+    Notiflix.Notify.success(
+      `We're sorry, but you've reached the end of search results.`
+    );
+  }
+
+  loadedImages = [...loadedImages, ...data.hits];
+  createMarkup(loadedImages);
+  // loading = false;
+}
+
+window.addEventListener('scroll', onScroll);
+
+function onScroll() {
+  const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 200) {
+    onButtonLoad();
+  }
+}
+
+onScroll();
